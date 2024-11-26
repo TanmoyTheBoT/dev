26f2c19f45f97ff89478a5a6482a0c949e16cfee
TanmoyTheBoT <tanmoysarkershuvo2@gmail.com>
2024-11-27 03:25:20 +0600

    feat: initial commit

--- _plugins/rename_sitemap.rb
+++ _plugins/rename_sitemap.rb
@@ -1,17 +1,16 @@
 Jekyll::Hooks.register :site, :post_write do |site|
-    # Define paths for the original sitemap and duplicates
-    sitemap_path = File.join(site.dest, "sitemap.xml")
-    sitemap_new_path = File.join(site.dest, "sitemap-new.xml")
-    sitemap_v2_path = File.join(site.dest, "sitemap-v2.xml")
-  
-    # Check if the main sitemap exists
-    if File.exist?(sitemap_path)
-      # Duplicate the sitemap
-      FileUtils.cp(sitemap_path, sitemap_new_path)
-      FileUtils.cp(sitemap_path, sitemap_v2_path)
-      puts "Duplicated sitemap.xml to sitemap-new.xml and sitemap-v2.xml"
-    else
-      puts "sitemap.xml not found, skipping duplication."
-    end
+  # Define paths for the original sitemap and renamed files
+  sitemap_path = File.join(site.dest, "sitemap.xml")
+  sitemap_new_path = File.join(site.dest, "sitemap-new.xml")
+  sitemap_v2_path = File.join(site.dest, "sitemap-v2.xml")
+
+  # Check if the main sitemap exists
+  if File.exist?(sitemap_path)
+    # Rename the sitemap to the new file names
+    FileUtils.mv(sitemap_path, sitemap_new_path)
+    FileUtils.cp(sitemap_new_path, sitemap_v2_path) # Copy from the renamed file to create another version
+    puts "Renamed sitemap.xml to sitemap-new.xml and created sitemap-v2.xml"
+  else
+    puts "sitemap.xml not found, skipping renaming."
   end
-  
+end
